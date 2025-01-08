88629554b13c8a5989f7a54d0e04213650e5883f
Trang Pham <trpham@c2r1p2.hive.fi>
2025-01-08 14:47:07 +0200

    commit makefile and main

--- putswap/push_swap.c
+++ putswap/push_swap.c
@@ -6,51 +6,105 @@
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 10:34:57 by trpham            #+#    #+#             */
-/*   Updated: 2025/01/07 13:13:06 by trpham           ###   ########.fr       */
+/*   Updated: 2025/01/08 14:46:30 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-t_node	*ft_lstnew(void *content);
+#include <stdint.h>
+
+static t_node	*ft_doubly_lstnew(int content);
+static int ft_is_valid_number(char	*str);
+
+// int	ft_atoi(const char *nptr)
+// {
+// 	int			i;
+// 	long long	result;
+// 	int			sign;
+
+// 	i = 0;
+// 	result = 0;
+// 	sign = 1;
+// 	while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
+// 		i++;
+// 	if (nptr[i] == '-' || nptr[i] == '+')
+// 	{
+// 		if (nptr[i] == '-')
+// 			sign = -1;
+// 		i++;
+// 	}
+// 	while (nptr[i] >= '0' && nptr[i] <= '9')
+// 	{
+// 		result = result * 10 + (nptr[i] - '0');
+// 		i++;
+// 	}
+// 	if (result * sign > INT_MAX)
+// 		return (-1);
+// 	else if (result * sign < INT_MIN)
+// 		return (0);
+// 	return (result * sign);
+// }
 
 int	main(int argc, char *argv[])
 {
-	int	i;
-	t_node *lst;
-	t_node *new;
-	t_node *current = NULL;
+	int		i;
+	t_node	*lst;
+	t_node	*new;
+	t_node	*current;
+	int		num;
 
-	i = 1;
+	i = 0;
 	lst = NULL;
+	current = NULL;
 	if (argc == 1)
-	{
 		return (1);
-	}
 	else if (argc > 1)
 	{
-		lst = ft_lstnew(argv[i]);
-		current = lst;
 		while (++i < argc)
 		{
-			new = ft_lstnew(argv[i]);
-			lst->next = new;
-			new->prev = lst;
-			lst = lst->next;
+			if (ft_is_valid_number(argv[i]) == 0)
+			{
+				num = ft_atoi(argv[i]);
+				new = ft_doubly_lstnew(num);
+				if (!lst)
+				{
+					lst = new;
+					current = lst;
+				}
+				else
+				{
+					lst->next = new;
+					new->prev = lst;
+					lst = lst->next;
+				}
+			}
+			else
+			{
+				printf("Error");
+				while (lst)
+				{
+					current = lst;
+					lst = lst->next;
+					free(current);
+				}
+				return (-1);
+			}
 		}
 	}
 	else
 		printf("Error: give a list of integer");
-
 	while (current)
 	{
-		printf("%s\n", current->content);
+		lst = current;
+		printf("%d\n", lst->content);
 		current = current->next;
+		free(lst);
+
 	}
-	
 	return (0);
 }
 
-t_node	*ft_lstnew(void *content)
+static t_node	*ft_doubly_lstnew(int content)
 {
 	t_node	*temp;
 
@@ -63,31 +117,19 @@ t_node	*ft_lstnew(void *content)
 	return (temp);
 }
 
-// void	ft_lstadd_back(t_node **lst, t_node *new)
-// {
-// 	t_node	*temp;
+static int ft_is_valid_number(char	*str)
+{
+	int	i;
 
-// 	if (!lst || !new)
-// 		return ;
-// 	if (!*lst)
-// 	{
-// 		*lst = new;
-// 		return ;
-// 	}
-// 	temp = *lst;
-// 	while (temp->next)
-// 	{
-// 		temp = temp->next;
-// 	}
-// 	temp->next = new;
-// }
-
-// void	ft_print_node(t_node *lst)
-// {
-// 	while (lst)
-// 	{
-// 		printf("%s", lst->content);
-// 		lst = lst->next;
-// 	}
-	
-// }
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '-' || str[i] == '+')
+			i++;
+		if (str[i] >= '0' && str[i] <= '9')
+			i++;
+		else
+			return (-1);
+	}
+	return (0);		
+}
--- putswap/push_swap.h
+++ putswap/push_swap.h
@@ -6,23 +6,23 @@
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 10:41:55 by trpham            #+#    #+#             */
-/*   Updated: 2025/01/07 13:26:43 by trpham           ###   ########.fr       */
+/*   Updated: 2025/01/08 13:36:14 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# include "./libft/libft.h"
 # include <stdlib.h>
 # include <stdio.h>
 
 typedef struct s_node
 {
-	char			*content;
-	int	index;
+	int				content;
+	int				index;
 	struct s_node	*next;
 	struct s_node	*prev;
-	
 }	t_node;
-t_node	*ft_lstnew(void *content);
+
 #endif
