16c1652f1bb1c0610c70f0c26ae9f11755e2ecb8
TrangPham93 <hatrangc2@gmail.com>
2025-01-13 18:15:31 +0200

    update swap.c

--- putswap/swap.c
+++ putswap/swap.c
@@ -6,7 +6,7 @@
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 16:09:00 by trpham            #+#    #+#             */
-/*   Updated: 2025/01/08 17:52:22 by trpham           ###   ########.fr       */
+/*   Updated: 2025/01/13 18:12:51 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,30 +28,34 @@ int	node_lst_size(t_node *lst)
 
 t_node *swap_stack(t_node *lst)
 {
-	// t_node	*head = lst;
-	int	first;
-	int	second;
 	int	len;
-	t_node	node1;
-	t_node	node2;
-	t_node	temp_node;
+	t_node	*node1 = NULL;
+	t_node	*node2 = NULL;
 
-	//check lst length
+	// check if the length is at least 2
+	if (lst == NULL || lst->next == NULL)
+		return (NULL);
+	
+	//check lst length, can remove
 	len = node_lst_size(lst);
 	printf("length of stack: %d\n", len);
 
-	if (len < 2)
-		return (lst);	
-	node1->next = lst;
-	// // current = lst;
-	// first = lst->content;
-	// lst = lst->next;
-	// second = lst->content;
-	// lst->content = first;
-	// lst = lst->prev;
-	// lst->content = second;
-	temp_node = node1->next;
+	// set value for node1 and node2
+	node1 = lst;
+	printf("Value of Node1 :%d\n",node1->content);
+	node2 = lst->next;
+	printf("Value of Node2 :%d\n",node2->content);
 
+	// swap, but need to ask for more robust option
+	node1->next = node2->next;
+    node2->next = node1;
+    node2->prev = NULL;
+    lst = node2;
+	return (lst);
+}
 
+t_node	*rotate_stack(t_node *lst)
+{
+	
 	return (lst);
 }
