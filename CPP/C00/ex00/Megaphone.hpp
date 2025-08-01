0a1bf64256d00d3bccb303d9367ba46f19fbc060
TrangPham93 <hatrangc2@gmail.com>
2025-08-01 13:38:36 +0300

    ex00 done

--- CPP/C00/ex00/Megaphone.hpp
+++ CPP/C00/ex00/Megaphone.hpp
@@ -6,13 +6,30 @@
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 11:30:41 by trpham            #+#    #+#             */
-/*   Updated: 2025/07/31 12:00:53 by trpham           ###   ########.fr       */
+/*   Updated: 2025/08/01 13:36:04 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef	MEGAPHONE_HPP
 # define MEGAPHONE_HPP
 
+# include <iostream>
 
+class	Megaphone{
+	public:
+		// Attribute
+		std::string MyString;
+
+		// Constructor
+		Megaphone(){
+			MyString = "";
+		};
+		
+		// Method
+		void		Capitalize(std::string &MyString);
+
+		// Destructor
+		~Megaphone(){};
+};
 
 #endif
--- CPP/C00/ex00/srcs/Megaphone.cpp
+++ CPP/C00/ex00/srcs/Megaphone.cpp
@@ -6,7 +6,37 @@
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/31 11:31:04 by trpham            #+#    #+#             */
-/*   Updated: 2025/07/31 11:31:05 by trpham           ###   ########.fr       */
+/*   Updated: 2025/08/01 13:36:56 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../Megaphone.hpp"
+
+void	Megaphone::Capitalize(std::string &MyString)
+{
+	if (!MyString.empty())
+	{
+		for (unsigned int i = 0; i < MyString.length(); i++)
+		{
+			if (MyString[i] >= 97 && MyString[i] <= 122)
+				MyString[i] -= 32;
+		};
+		
+	}
+}
+
+int	main(int ac, char **av)
+{
+	Megaphone	MyObj;
+		
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+	for (int i = 1; i < ac; i++)
+		MyObj.MyString += av[i];
+	MyObj.Capitalize(MyObj.MyString);
+	std::cout << MyObj.MyString << std::endl;
+	return 0;
+}
