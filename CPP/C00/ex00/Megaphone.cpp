c045b2a15ae908a84ed46b4f809d61dc9da04239
TrangPham93 <hatrangc2@gmail.com>
2025-08-01 15:39:21 +0300

    fix ex00

--- CPP/C00/ex00/Megaphone.cpp
+++ CPP/C00/ex00/Megaphone.cpp
@@ -1,17 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Megaphone.hpp                                      :+:      :+:    :+:   */
+/*   Megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: trpham <trpham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/31 11:30:41 by trpham            #+#    #+#             */
-/*   Updated: 2025/08/01 13:36:04 by trpham           ###   ########.fr       */
+/*   Created: 2025/07/31 11:31:04 by trpham            #+#    #+#             */
+/*   Updated: 2025/08/01 15:31:25 by trpham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#ifndef	MEGAPHONE_HPP
-# define MEGAPHONE_HPP
 
 # include <iostream>
 
@@ -24,12 +21,22 @@ class	Megaphone{
 		Megaphone(){
 			MyString = "";
 		};
-		
-		// Method
-		void		Capitalize(std::string &MyString);
-
-		// Destructor
-		~Megaphone(){};
 };
 
-#endif
+int	main(int ac, char **av)
+{
+	Megaphone	MyObj;
+		
+	if (ac == 1){
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+	
+	for (int i = 1; i < ac; i++)
+		MyObj.MyString += av[i];
+	for (unsigned int index = 0; index < MyObj.MyString.length(); index++)
+		std::cout << char(std::toupper(MyObj.MyString[index]));
+	std::cout << std::endl;
+	
+	return 0;
+}
